4925c50e46a2bc93ef8ef8c960dd7280e363d65f
TMcClure123 <jumpscares4days@gmail.com>
2024-05-28 13:44:41 -0400

    trying to make cards appear

--- script.js
+++ script.js
@@ -1,39 +1,5 @@
-function openCardPack() {
-    var largeCardPack = document.getElementById('large-card-pack');
-    var smallCardPacksContainer = document.getElementById('small-card-packs');
-  
-    // Check if large card pack exists
-    if (!largeCardPack) {
-      console.error('Large card pack not found!');
-      return;
-    }
-  
-    // Remove the large card pack
-    largeCardPack.remove();
-  
-    // Create smaller card packs
-    for (var i = 1; i <= 3; i++) {
-      var smallCardPack = document.createElement('div');
-      smallCardPack.className = 'card-pack';
-      
-      // Create an image button for opening the pack
-      var openButton = document.createElement('img');
-      openButton.className = 'open-button';
-      openButton.onclick = function() {
-        this.parentNode.remove(); // Remove the parent (the small card pack) when the image is clicked
-      };
-      
-      smallCardPack.appendChild(openButton);
-      
-      for (var j = 1; j <= 3; j++) {
-        var card = document.createElement('div');
-        card.className = 'card';
-        card.innerText = 'Card ' + ((i - 1) * 3 + j);
-        smallCardPack.appendChild(card);
-      }
-      smallCardPacksContainer.appendChild(smallCardPack);
-    }
-  }
+
+
   document.getElementById('pauseButton').addEventListener('click', function() {
     var audio = document.getElementById('backgroundAudio');
     if (audio.paused) {
