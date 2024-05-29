ea8441dd7aee0bbc753605dce5ce463f91f77a81
TMcClure123 <jumpscares4days@gmail.com>
2024-05-29 14:26:38 -0400

    Cards Fixed

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleCards = document.getElementById('toggleCards');
+  const cardPack = document.getElementById('large-card-pack');
+  const pauseButton = document.getElementById('pauseButton');
+  const backgroundAudio = document.getElementById('backgroundAudio');
+});
+  cardPack.addEventListener('click', function() {
+    toggleCards.checked = !toggleCards.checked;
+  });
 
   document.getElementById('pauseButton').addEventListener('click', function() {
     var audio = document.getElementById('backgroundAudio');
